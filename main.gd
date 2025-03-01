3e492993fdfc99ae3aa0a04584e9fdb4b6ce43e7
William Burton <13026711+burtonwilliamt@users.noreply.github.com>
2025-03-01 16:38:56 -0500

    Host button spawns player

--- main.gd
+++ main.gd
@@ -1,10 +1,36 @@
 extends Node2D
 
+var peer = ENetMultiplayerPeer.new()
+const port = 4242
+
+var players = []
+
+@export var player_container_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
+func _on_host_pressed():
+	var error = peer.create_server(int(port))
+	if error:
+		return error
+	multiplayer.multiplayer_peer = peer
+	
+	_on_connected()
+	_add_player(1)
+
+func _on_connected():
+	_toggle_menu(false)
+
+func _toggle_menu(state: bool):
+	$MainMenu.visible=state
+
+func _add_player(_id: int):
+	var player = player_container_scene.instantiate() as Player
+	player.position = Vector2(100,100)
+	players.append(player)
+	$EntityContainer.add_child(player)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- player.gd
+++ player.gd
@@ -1,5 +1,4 @@
-extends CharacterBody2D
-
+class_name Player extends CharacterBody2D
 
 const SPEED = 300.0
 
